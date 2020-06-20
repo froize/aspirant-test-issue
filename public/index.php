a9ccc8b37ac989e50577ef1e59b4aee71acbdecd
Ali <froize@yandex.ru>
2020-06-20 23:31:45 +0500

    original task

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<?php
+
+/** @var ContainerInterface $container */
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Slim\App;
+use Slim\Interfaces\CallableResolverInterface;
+use Slim\Interfaces\RouteCollectorInterface;
+use Slim\Interfaces\RouteResolverInterface;
+use Slim\Middleware\ErrorMiddleware;
+use Slim\Middleware\RoutingMiddleware;
+use Slim\Psr7\Factory\ServerRequestFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+
+$container = require dirname(__DIR__) . '/bootstrap.php';
+$request = ServerRequestFactory::createFromGlobals();
+
+Slim\Factory\AppFactory::create();
+
+$app = new App(
+    $container->get(ResponseFactoryInterface::class),
+    $container,
+    $container->get(CallableResolverInterface::class),
+    $container->get(RouteCollectorInterface::class),
+    $container->get(RouteResolverInterface::class)
+);
+
+$app->add($container->get(RoutingMiddleware::class));
+$app->add($container->get(ErrorMiddleware::class));
+
+
+
+$app->run($request);
+
+
